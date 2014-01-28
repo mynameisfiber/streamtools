70b376c8c49dc6338ea3e20a85359134b6daaf14
Micha Gorelick <mynameisfiber@gmail.com>
2014-01-28 16:35:04 -0500

    block?

--- blocks/hll.go
+++ blocks/hll.go
@@ -1,111 +1,43 @@
 package blocks
-// Current implemintation if from: http://algo.inria.fr/flajolet/Publications/FlFuGaMe07.pdf
-// Future: implement this instead -- http://static.googleusercontent.com/external_content/untrusted_dlcp/research.google.com/en/us/pubs/archive/40671.pdf
 
 import (
-	"errors"
-	"github.com/reusee/mmh3"
+	"github.com/mynameisfiber/gohll"
 	"log"
-	"math"
+    "fmt"
 )
-
-var (
-	// Various errors that can be thrown
-	InvalidRuleB = errors.New("Invalid value of `b`.  4 <= b <= 16")
-)
-
-var (
-    // constants
-    _2Raised32 = float64(1 << 32)
-    _2Raised32Over30 = float64(1 << 32) / 30.0
-    _5Halfs = float64(5.0 / 2.0)
-)
-
-// the rule defines how the block works. It can be get and set live
-type hllRule struct {
-	B        uint8
-	Key      string
-}
 
 // the state defines the current state of the block. This is where we store
 // whatever is being learned about the data
 type hllState struct {
-	M     []uint8
-	Alpha float64
-	rule  *hllRule
+    Hlls map[string]*gohll.HLL
+    rule *hllRule
 }
+
+func NewHLLState(rule *hllRule) *hllState {
+    return &hllState{
+        Hlls: make(map[string]*gohll.HLL),
+        rule: rule,
+    }
+}
+
+// the rule defines how the block works. It can be get and set live
+type hllRule struct {
+	ErrorRate float64
+	Key      string
+	Value    string
+}
+
 
 func (h *hllState) Add(key, value string) {
-	hash := mmh3.Hash32([]byte(value))
-	j, w := SplitInt(hash, h.rule.B)
-	rho := LeftmostSetBit(w) - uint8(h.rule.B)
-    log.Printf("hash: %b\n", hash)
-    log.Printf("j:    %b\n", j)
-    log.Printf("w:    %b\n", w)
-    log.Printf("rho:  %d\n\n", rho)
-	if h.M[j] < rho {
-		h.M[j] = rho
-	}
-}
-
-func (h *hllState) Cardinality() float64 {
-    m := float64(len(h.M))
-    ETop := h.Alpha * m * m
-    EBottom := 0.0
-    V := 0.0
-    for _, value := range h.M {
-        EBottom += math.Pow(2, -1.0 * float64(value))
-        if value == 0 {
-            V += 1.0
-        }
-    }
-    E := ETop / EBottom
-    
-    switch {
-        case E <= _5Halfs * m:
-            if V == 0.0 {
-                return E
-            }
-            return m * math.Log2(m / V)
-        case E <= _2Raised32Over30:
-            return E
-        default:
-            return -1.0 * _2Raised32 * math.Log2(1 - E / _2Raised32)
+    fmt.Println("Adding value: ", value)
+    if hll, ok := h.Hlls[key]; ok {
+        hll.Add(value)
+    } else {
+        h.Hlls[key], _ = gohll.NewHLLByError(h.rule.ErrorRate)
+        h.Hlls[key].Add(value)
     }
 }
 
-func LeftmostSetBit(n uint32) uint8 {
-	// Returns the index of the leftmost set bit
-	return 32 - uint8(math.Log2(float64(n))+1)
-}
-
-func SplitInt(N uint32, index uint8) (leading, trailing uint32) {
-	offset := uint(32 - index)
-	leading = N >> offset
-	trailing = N ^ (leading << offset)
-	return leading, trailing
-}
-
-func NewHLLState(rule *hllRule) (*hllState, error) {
-	if rule.B < 4 || rule.B > 16 {
-		return nil, InvalidRuleB
-	}
-	m := uint(math.Pow(2, float64(rule.B)))
-	M := make([]uint8, m)
-	var alpha float64
-	switch m {
-	case 16:
-		alpha = 0.673
-	case 32:
-		alpha = 0.697
-	case 64:
-		alpha = 0.709
-	default:
-		alpha = 0.7213 / (1.0 + 1.079/float64(m))
-	}
-	state := hllState{M: M, Alpha: alpha, rule: rule}
-	return &state, nil
-}
 
 // this is a skeleton state block. It doesn't do anything, but can be used as a
 // template to make new state blocks.
@@ -119,9 +51,11 @@ func HLL(b *Block) {
 		select {
 		case query := <-b.Routes["cardinality"]:
 			// the state route is used to retrieve the state of the block
-            cardinality := data.Cardinality()
-            log.Println("Found cardinality: ", cardinality)
-            marshal(query, map[string]float64{"cardinality":cardinality})
+            result := make(map[string]float64)
+            for key, hll := range data.Hlls {
+                result[key] = hll.Cardinality()
+            }
+            marshal(query, result)
 		case ruleUpdate := <-b.Routes["set_rule"]:
 			// the set rule route is used to update the block's rule. There are two
 			// situations: if we've not seen any rules before - i.e. this is a brand
@@ -132,9 +66,9 @@ func HLL(b *Block) {
                 log.Println("HLL: Could not parse new rule")
                 continue
             }
-            if rule == nil || newRule.B != rule.B {
+            if rule == nil || newRule.ErrorRate != rule.ErrorRate {
 				var err error
-				data, err = NewHLLState(newRule)
+				data = NewHLLState(newRule)
 				if err != nil {
 					// if a bad rule set gets sent in, log to STDOUT and un-initialize this block
                     log.Println("HLL: Error setting state for new rule: ", err.Error())
@@ -166,18 +100,18 @@ func HLL(b *Block) {
 				break
 			}
 
-			valueSlice := getKeyValues(msg.Msg, rule.Key)
+			valueSlice := getKeyValues(msg.Msg, rule.Value)
 			// we need to guard against the possibility that the key is not in
 			// the message
 			if len(valueSlice) == 0 {
-                log.Println("HLL: could not find", rule.Key, "in message: ", msg)
+                log.Println("HLL: could not find", rule.Value, "in message: ", msg)
 				break
 			}
 
 			value := valueSlice[0]
 			valueString, ok := value.(string)
 			if !ok {
-                log.Println("HLL: nil value against", rule.Key, " - ignoring")
+                log.Println("HLL: nil value against", rule.Value, " - ignoring")
 				break
 			}
             data.Add("", valueString)
---
+++ test_hll.sh
@@ -0,0 +1,16 @@
+curl "localhost:7070/create?blockType=getHTTP&id=citibike"
+curl localhost:7070/blocks/citibike/set_rule -d '{"Endpoint":"http://citibikenyc.com/stations/json"}'
+curl "localhost:7070/create?blockType=ticker&id=t"
+curl localhost:7070/blocks/t/set_rule -d '{"Period":4}'
+curl "localhost:7070/connect?from=t&to=citibike"
+curl "localhost:7070/create?blockType=unpack&id=unpacker"
+curl localhost:7070/blocks/unpacker/set_rule -d '{"Path":"stationBeanList[]"}'
+curl "localhost:7070/connect?from=citibike&to=unpacker"
+
+#curl "localhost:7070/create?blockType=tolog&id=l"
+#curl "localhost:7070/connect?from=unpacker&to=l"
+
+curl "localhost:7070/create?blockType=hll&id=hll"
+curl localhost:7070/blocks/hll/set_rule -d '{"Value":"stAddress1","ErrorRate":0.001}'
+curl "localhost:7070/connect?from=unpacker&to=hll"
+
